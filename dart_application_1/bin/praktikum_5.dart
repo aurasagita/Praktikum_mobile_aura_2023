959e314e2d6e7ee60e25b2165e4ea1ae60110baa
usernameAnda <aurasagita2122gmail.com>
2023-10-10 19:47:22 +0700

    fixing_1

--- dart_application_1/bin/praktikum_5.dart
+++ dart_application_1/bin/praktikum_5.dart
@@ -9,11 +9,12 @@ void main(List<String> arguments) {
   print(tukar(record));*/
 
   /*(String, int) mahasiswa;
-  mahasiswa=('Aura Sagita Ashilah', 2141720201);
+  mahasiswa=('Aura Sagita Ashilah', 2141720102);
   print(mahasiswa);*/
 
 var mahasiswa2 = ('Aura Sagita Ashilah', a: 2141720102, b: true, 'last');
 print(mahasiswa2.$1); // Prints 'first'
 print(mahasiswa2.a); // Prints 2
+print(mahasiswa2.b);
 
 }
